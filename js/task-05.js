e861b0f073558f39ab039fcfaf800f8e95ca17e6
Vadym Starchenko <starvadim@ukr.net>
2022-02-03 09:54:25 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,10 @@
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
-nameInput.addEventListener('input', (event) => {
-    event.preventDefault();
-    if (!event.currentTarget.value) {
-        nameOutput.textContent = 'Anonymous';
+nameInput.addEventListener('input', () => {
+    const inputValue= nameInput.value.trim()
+     nameOutput.textContent = inputValue||'Anonymous';
         return;
     }
-    nameOutput.textContent = event.currentTarget.value;
-});
+);
 
