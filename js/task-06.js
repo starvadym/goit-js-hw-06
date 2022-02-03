34ca1a0d19544c92416fde4407d850db042e395c
Vadym Starchenko <starvadim@ukr.net>
2022-02-03 10:11:43 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,8 +4,10 @@ refsInputNumber.addEventListener("blur", () => {
 
    if (refsInputNumber.value.length === Number(refsInputNumber.dataset.length)) {
         refsInputNumber.classList.add('valid');
+        refsInputNumber.classList.remove('invalid');
     } else {
      refsInputNumber.classList.add('invalid');
+     refsInputNumber.classList.remove('valid');
     }
 
 });
